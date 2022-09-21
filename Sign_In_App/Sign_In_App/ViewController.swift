63747b9e2213cc5814f36a5e8a99df3d3d6a8ae3
BurucaJamuel-FS <83684235+BurucaJamuel-FS@users.noreply.github.com>
2022-09-21 15:08:54 -0400

    this is the start of the app process

---
+++ Sign_In_App/Sign_In_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sign_In_App
+//
+//  Created by jamuel buruca on 9/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
