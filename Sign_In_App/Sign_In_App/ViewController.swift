57123f0b7ea0eaacd19f0c10baa5bfdd0ed4b9a6
BurucaJamuel-FS <83684235+BurucaJamuel-FS@users.noreply.github.com>
2022-09-21 17:37:15 -0400

    added some button functinlity to be ablt to switch between sign in and sign up

--- Sign_In_App/Sign_In_App/ViewController.swift
+++ Sign_In_App/Sign_In_App/ViewController.swift
@@ -9,14 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var users = Array<UserInfo>()
-
+    //this is an array for the users info
+    var users = [UserInfo]()
+    
+    //here i will have the outlets for the textFields
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var password1: UITextField!
+    @IBOutlet weak var passwordReenter: UITextField!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+   
+    //these are the buttons outlets
+    @IBOutlet weak var signInOut: UIButton!
+    @IBOutlet weak var signUpOut: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
+    
+    
 
-
+//this will be the action for the sign up and sign in feature
+    //i will use a tag to seprate the buttons
+    @IBAction func SignINSignUp(_ sender: UIButton) {
+        switch(sender.tag){
+        case 0:
+            //this changes the buttons selected
+            signInOut.isSelected = true
+            signUpOut.isSelected = false
+            
+            passwordReenter.isHidden = true
+            firstName.isHidden = true
+            lastName.isHidden = true
+            break
+        case 1:
+            //this changes the buttons selected
+            signInOut.isSelected = false
+            signUpOut.isSelected = true
+            
+            passwordReenter.isHidden = false
+            firstName.isHidden = false
+            lastName.isHidden = false
+            
+            
+            break
+        default:
+            print("there was an error somewhere")
+        }
+    }
+    
+    
+    @IBAction func Submit(_ sender: UIButton) {
+    }
 }
 
