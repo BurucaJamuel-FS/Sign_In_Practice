f539a09ba07775ed2206e065160546d8755993da
BurucaJamuel-FS <83684235+BurucaJamuel-FS@users.noreply.github.com>
2022-09-25 09:38:10 -0400

    added another screen to add the users info to that screen and need to work on a sign out feature

---
+++ Sign_In_App/Sign_In_App/SecoundViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecoundViewController.swift
+//  Sign_In_App
+//
+//  Created by jamuel buruca on 9/25/22.
+//
+
+import UIKit
+
+class SecoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
